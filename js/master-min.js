d29d3f56ced4102e0d94ad05a4260f5ae4f10161
Joe Romero <joethemovie@gmail.com>
2017-07-11 18:01:59 -0400

    initial commit

---
+++ js/master-min.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $("#slideshow > div:gt(0)").hide();
+    setInterval(function() {
+        $("#slideshow > div:first").fadeOut(1e3).next().fadeIn(1e3).end().appendTo("#slideshow");
+    }, 3e3);
+    $("#slideshow2 > div:gt(0)").hide();
+    setInterval(function() {
+        $("#slideshow2 > div:first").fadeOut(1e3).next().fadeIn(1e3).end().appendTo("#slideshow2");
+    }, 3e3);
+    $("#slideshow3 > div:gt(0)").hide();
+    setInterval(function() {
+        $("#slideshow3 > div:first").fadeOut(1e3).next().fadeIn(1e3).end().appendTo("#slideshow3");
+    }, 3e3);
+    $("#slideshow4 > div:gt(0)").hide();
+    setInterval(function() {
+        $("#slideshow4 > div:first").fadeOut(1e3).next().fadeIn(1e3).end().appendTo("#slideshow4");
+    }, 3e3);
+    $("#slideshow6 > div:gt(0)").hide();
+    setInterval(function() {
+        $("#slideshow6 > div:first").fadeOut(1e3).next().fadeIn(1e3).end().appendTo("#slideshow6");
+    }, 3e3);
+    $("#slideshow7 > div:gt(0)").hide();
+    setInterval(function() {
+        $("#slideshow7 > div:first").fadeOut(1e3).next().fadeIn(1e3).end().appendTo("#slideshow7");
+    }, 3e3);
+    $("#slideshow8 > div:gt(0)").hide();
+    setInterval(function() {
+        $("#slideshow8 > div:first").fadeOut(1e3).next().fadeIn(1e3).end().appendTo("#slideshow8");
+    }, 3e3);
+});
